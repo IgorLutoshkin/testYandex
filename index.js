0d33081a9e482321929b1cc02a75aa8b80785deb
IgorLutoshkin <ig.yarij@mail.ru>
2024-03-27 09:30:55 +0300

    add animation right button for participants

--- index.js
+++ index.js
@@ -73,6 +73,169 @@ moveItemsBasedOnScreenWidth_2();
 
 window.addEventListener("resize", moveItemsBasedOnScreenWidth_2);
 
+/*  */
+const data = [
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Хозе-Рауль Капабланка",
+    status: "Чемпион мира по шахматам",
+  },
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Эммануил Ласкер",
+    status: "Чемпион мира по шахматам",
+  },
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Александр Алехин",
+    status: "Чемпион мира по шахматам",
+  },
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Арон Нимцович",
+    status: "Чемпион мира по шахматам",
+  },
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Рихард Рети",
+    status: "Чемпион мира по шахматам",
+  },
+  {
+    avatar: "./img/image-gamer.png",
+    name: "Остап Бендер",
+    status: "Чемпион мира по шахматам",
+  },
+];
+
+/*  */
+const container = document.querySelector(".participants");
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+let scrollPosition = 0;
+let activeParticipants = 1;
+const widthOffset = 414;
+let flag = true;
+/*  */
 
 /*  */
 
+const listElement = document.createElement("ul");
+listElement.className = "participants-list";
+container.append(listElement);
+/*  */
+function render(indexArr) {
+  const itemElement = document.createElement("li");
+  itemElement.className = "participants-item";
+
+  /*  */
+  const pictureElement = document.createElement("picture");
+  itemElement.append(pictureElement);
+
+  /*  */
+  const sourceElement = document.createElement("source");
+  sourceElement.srcset = "./img/image-gamer-1180.png";
+  sourceElement.media = "(max-width:1180px)";
+  /*  */
+  const imgElement = document.createElement("img");
+  imgElement.className = "participants-avatar";
+  imgElement.src = data[indexArr].avatar;
+
+  /*  */
+  const nameElement = document.createElement("p");
+  nameElement.className = "participant-name";
+  nameElement.innerText = data[indexArr].name;
+  /*  */
+  const statusElement = document.createElement("p");
+  statusElement.className = "participant-status";
+  statusElement.innerText = data[indexArr].status;
+  /*  */
+  const buttonElement = document.createElement("button");
+  buttonElement.className = "participant-about-btn";
+  buttonElement.innerText = "Подробнее";
+  /*  */
+  listElement.append(itemElement);
+
+  pictureElement.append(sourceElement, imgElement);
+  itemElement.append(pictureElement, nameElement, statusElement, buttonElement);
+
+  return itemElement;
+}
+render(activeParticipants);
+/*  */
+/*  */
+function rightImage() {
+  let rightImg = activeParticipants + 1;
+  if (rightImg >= data.length) rightImg = 0;
+  listElement.append(render(rightImg));
+}
+rightImage();
+/*  */
+/*  */
+/*  */
+function leftImage() {
+  let leftImg = activeParticipants - 1;
+  if (leftImg < 0) leftImg = data.length - 1;
+  listElement.prepend(render(leftImg));
+}
+leftImage();
+/*  */
+/*  */
+/*  */
+leftBtn.addEventListener("click", () => {
+  activeParticipants--;
+  if (activeParticipants < 0) activeParticipants = data.length - 1;
+
+  const containerList = document.querySelector(".participants-list");
+  const newElement = render(activeParticipants);
+
+  animate({
+    duration: 1500,
+    draw: function (progress) {
+      const newPosition = widthOffset * progress;
+
+      containerList.style.transform = `translateX(${newPosition}px)`;
+      newElement.style.transform = `translateX(${newPosition}px)`; // Изменено на обратное движение
+    },
+    removeElement: containerList.lastChild,
+    insertElement: newElement,
+  });
+
+  leftImage();
+});
+
+rightBtn.addEventListener("click", () => {
+  activeParticipants++;
+  if (activeParticipants >= data.length) activeParticipants = 0;
+
+  const containerList = document.querySelector(".participants-list");
+  const newElement = render((activeParticipants + 1) % data.length);
+  animate({
+    duration: 1500,
+    draw: function (progress) {
+      const newPosition = -widthOffset * progress;
+
+      containerList.style.transform = `translateX(${newPosition}px)`;
+      newElement.style.transform = `translateX(-${newPosition}px)`;
+    },
+    removeElement: containerList.firstChild,
+    insertElement: newElement,
+  });
+});
+/*  */
+/*  */
+/*  */
+const animate = ({ duration, draw, removeElement }) => {
+  const start = performance.now();
+  requestAnimationFrame(function animate(time) {
+    let step = (time - start) / duration;
+    if (step > 1) step = 1;
+    draw(step);
+    if (step < 1) {
+      requestAnimationFrame(animate);
+    } else {
+      removeElement.remove();
+
+      document.querySelector(".participants-list").style.transform = ""; // Reset transform
+    }
+  });
+};
