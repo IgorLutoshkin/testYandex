7901393bc6d17b5687b15db52b63b39cca30840c
IgorLutoshkin <ig.yarij@mail.ru>
2024-04-07 23:49:45 +0300

    add counter for parents

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ const data = [
 ];
 
 /*  */
+const counterElement = document.querySelector(".count-participants");
 const container = document.querySelector(".participants");
 const leftBtn = document.querySelector(".left");
 const rightBtn = document.querySelector(".right");
@@ -161,12 +162,14 @@ function render(indexArr) {
   return itemElement;
 }
 render(activeParticipants);
-/*  */
-/*  */
+
+/* ***************************************************** */
 function rightImage() {
   let rightImg = activeParticipants + 1;
   if (rightImg >= data.length) rightImg = 0;
   listElement.append(render(rightImg));
+
+  rightCount(activeParticipants);
   return rightImg;
 }
 rightImage();
@@ -177,12 +180,13 @@ function leftImage() {
   let leftImg = activeParticipants - 1;
   if (leftImg < 0) leftImg = data.length - 1;
   listElement.prepend(render(leftImg));
+  leftCount(activeParticipants);
   return leftImg;
 }
 leftImage();
 /*  */
 /*  */
-/*  */
+/* ********************************************** */
 leftBtn.addEventListener("click", () => {
   activeParticipants--;
   if (activeParticipants < 0) activeParticipants = data.length - 1;
@@ -190,6 +194,7 @@ leftBtn.addEventListener("click", () => {
   const containerList = document.querySelector(".participants-list");
   const computedStyles = window.getComputedStyle(containerList);
   const gapValue = computedStyles.getPropertyValue("gap");
+
   leftImage();
   animate({
     duration: 1000,
@@ -212,6 +217,7 @@ rightBtn.addEventListener("click", () => {
 
   const computedStyles = window.getComputedStyle(containerList);
   const gapValue = computedStyles.getPropertyValue("gap");
+  counterElement.innerText = "";
 
   rightImage();
   animate({
@@ -264,3 +270,115 @@ window.addEventListener("resize", function () {
     document.querySelector(".participants-list").style.gap = newGap + "px";
   }
 });
+/* ******************************************************** */
+
+/*  */
+function leftCount(activeParticipants) {
+  let countElementPage = listElement.childElementCount;
+  let screenWidth_1 = 1126;
+  let screenWidth_2 = 729;
+
+  /* экраны до 1126, когда отображается 3 участника */
+
+  if (window.innerWidth > screenWidth_1) {
+    let rangeStart = activeParticipants + 2;
+    let countElementPage = listElement.childElementCount;
+
+    if (rangeStart <= countElementPage) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+
+    if (rangeStart > data.length) {
+      counterElement.innerText = rangeStart - data.length + "/" + data.length;
+    }
+    if (rangeStart <= data.length) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+  }
+  /* экраны от 1126, когда отображается 2 участника */
+  if (window.innerWidth <= screenWidth_1) {
+    let rangeStart = activeParticipants + 1;
+
+    if (rangeStart <= countElementPage) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+
+    if (rangeStart > data.length) {
+      counterElement.innerText = rangeStart - data.length + "/" + data.length;
+    }
+    if (rangeStart <= data.length) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+  }
+
+  /* экраны от 729, когда отображается 1 участни */
+
+  if (window.innerWidth <= screenWidth_2) {
+    let rangeStart = activeParticipants;
+
+    if (rangeStart <= countElementPage) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+
+    if (rangeStart < data.length) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+    if (rangeStart === 0) {
+      rangeStart = data.length;
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+  }
+}
+function rightCount(activeParticipants) {
+  let countElementPage = listElement.childElementCount;
+  let screenWidth_1 = 1126;
+  let screenWidth_2 = 729;
+
+  /* экраны до 1126, когда отображается 3 участника */
+
+  let rangeEnd = activeParticipants + 2;
+
+  if (rangeEnd > data.length) {
+    rangeEnd -= data.length;
+  }
+
+  if (rangeEnd === 0) {
+    rangeEnd = data.length;
+  }
+
+  counterElement.innerText = rangeEnd + "/" + data.length;
+
+  /* экраны от 1126, когда отображается 2 участника */
+  if (window.innerWidth <= screenWidth_1) {
+    let rangeStart = activeParticipants + 1;
+
+    if (rangeStart <= countElementPage) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+
+    if (rangeStart > data.length) {
+      counterElement.innerText = rangeStart - data.length + "/" + data.length;
+    }
+    if (rangeStart <= data.length) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+  }
+
+  /* экраны от 729, когда отображается 1 участни */
+
+  if (window.innerWidth <= screenWidth_2) {
+    let rangeStart = activeParticipants;
+
+    if (rangeStart <= countElementPage) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+
+    if (rangeStart < data.length) {
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+    if (rangeStart === 0) {
+      rangeStart = data.length;
+      counterElement.innerText = rangeStart + "/" + data.length;
+    }
+  }
+}
